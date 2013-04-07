3af7882a2b1f86c759a1dd0bbe6e2a21b4db8a9f
chris williams <cgw239@nyu.edu>
2013-04-07 02:02:10 -0400

    decryption kinda sorta does something half right

--- Decrypt.py
+++ Decrypt.py
@@ -1,12 +1,29 @@
 import math
 import string
 import sys
+import csv
 
 from sys import argv
 
-script, in_string, in_key = argv
+script, in_put, in_key = argv
 
-def decrypt(encrypted, key):
-	return encrypted
+#def decrypt(encrypted, key):
+#	for row in spamreader:
+#		print encrypted[:row]
+#		print ''.join(row)
+#	print encrypted[:1]
+#	print key
+#	return "whatever"
 
-print decrypt(in_string, in_key)
+encrypted = open(in_put).read()
+out = ''
+i = 0
+with open(in_key, 'rb') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	for row in spamreader:
+		for num in row:
+			out += encrypted[i:i+1]
+			i += int(num)
+
+print out
+#print decrypt(encrypted, key)
