8445a1e37a10d58e9444c8e9d69def2f8d7cf56c
chris williams <cgw239@nyu.edu>
2013-04-07 02:08:45 -0400

    fixed decrypt, should work correctly now

--- Decrypt.py
+++ Decrypt.py
@@ -5,8 +5,8 @@ encrypted = open(in_put).read()
 out = ''
 i = 0
 with open(in_key, 'rb') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-	for row in spamreader:
+	reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	for row in reader:
 		for num in row:
 			out += encrypted[i:i+1]
 			i += int(num) + 1
